295cb11032293db56c646d28fc76d789dac71651
AmirabbasAfrasiyabi <amirabbasafrasiyabi1998@gmail.com>
2026-07-20 11:50:07 +0330

    updated

---
+++ CS50's Introduction to Computer Science/C/0-1/main.c
@@ -0,0 +1,40 @@
+//Question1
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("hello, world\n");
+// }
+
+
+
+//Question2
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("hello, world");
+// }
+
+
+
+//Question3
+// #include <stdio.h>
+// int main(void)
+// {
+//     char name[50];
+//     printf("what is your names ??");
+//     fgets(name , sizeof(name) , stdin);
+//     printf("Hello %s",name);
+// }
+
+
+
+
+//Question4 
+#include <stdio.h>
+
+int main(void)
+
+{
+    int x = 
+}
